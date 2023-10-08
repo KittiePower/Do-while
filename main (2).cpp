041ad4c2d07c3e02b065166f3a8c018e37adc717
Elisa Medina <147352316+KittiePower@users.noreply.github.com>
2023-10-08 16:51:14 -0700

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+Do-while
+Nombre: Elisa Michelle Medina Vázquez 
+Fecha: 090522
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+int eleccion;
+
+do{
+std::cout<<"Seleccione el numero de la operacion que desea realizar: "<<std::endl;
+std::cout<<""<< std::endl;
+std::cout<<"1) Suma" <<std::endl;
+std::cout<<"2) Resta"<<std::endl;
+std::cout<<"3) Multiplicacion"<<std::endl;
+std::cout<<"4) Salida"<<std::endl;
+
+cout<<"Operacion: ";
+std::cin>>eleccion;
+
+std::cout<<"" <<std::endl;
+if(eleccion<=0||eleccion>4){
+std::cout<<"***** OPCION INVALIDA *****" << std::endl;
+std::cout<<""<< std::endl;
+}
+
+}while(eleccion<=0||eleccion>4);
+if(eleccion== 1){
+std::cout<<"-----SUMA-----"<< std::endl;
+
+    }
+    
+    return 0;
+}
+
+
